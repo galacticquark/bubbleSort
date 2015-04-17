18e2b3545e2927d9836dec392ab41ad9b0b4f6ec
GalacticQuark <GalacticQuark@gmail.com>
2015-04-16 20:06:56 -0500

    add arraylist with values to sort

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -18,4 +18,18 @@ public class bubbleSort {
 		}
 		return array;
 	}
+	public static void main(String[] args){
+		bubbleSort mySort = new bubbleSort();
+		ArrayList<Number> myList= new ArrayList<Number>();
+		myList.add(10);
+		myList.add(0.000001);
+		myList.add(10.0);
+		myList.add((long)10);
+		myList.add((short)1.2);		
+		myList.add((float)10);
+		myList.add((byte)5);
+		System.out.println(mySort.sort(myList));
+		
+		
+		}
 }
